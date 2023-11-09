32d0c25bb3d2681a9eb8717ea0c01e05bd2b78a8
Elmo <elmo.heine@nordholz-edv.de>
2023-11-09 12:19:01 +0100

    Removed unused code and refactored some functions

--- src/app/grouped-mat-table/grouped-mat-table.component.ts
+++ src/app/grouped-mat-table/grouped-mat-table.component.ts
@@ -15,55 +15,20 @@ export class NhGroupedMatTable {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   @Input() filter?: { show: boolean, placeholder?: string };
-  @Input() pager?: { pageSizeOptions?: number[], defaultPageSize?: number, position?: "top" | "bottom" | "disabled" }
+  // @Input() pager?: { pageSizeOptions?: number[], defaultPageSize?: number, position?: "top" | "bottom" | "disabled" }
   @Input({ required: true }) columnOrder!: Column<any>[];
   @Input({ required: true }) data!: Group<ItemData>[];
   @Input() showIndex: "normal" | "relative" | "both" | "none" = "none";
   
-  _data: (Item | GroupItem)[] = [];
+  _data: (DataItem | GroupItem)[] = [];
 
   displayedColumnOrder: string[] = [];
 
   dataSource!: MatTableDataSource<any>;
 
   ngOnInit() {
-    let id = 0;
-    let itemId = 0;
-    for (let i = 0; i < this.data.length; ++i) {
-      const group = this.data[i];
-      const groupId = id;
-      // console.log({ itemId, groupId });
+    this.initData(this.data);
 
-      this._data.push(
-        new GroupItem(
-          i,
-          group.name,
-          group.isExpanded,
-          id,
-          group.items.length
-        )
-      );
-
-      for (let j = 0; j < group.items.length; ++j) {
-        ++id;
-        
-        const item: Item = {
-          data: group.items[j],
-          groupId,
-          id: id,
-          isExpanded: true,
-          itemId,
-          relativeIndex: j
-        };
-
-        this._data.push(item);
-        ++itemId;
-      }
-      ++id;
-    }
-
-    // console.log(this._data)
-    
     this.dataSource = new MatTableDataSource(this._data);
 
     this.displayedColumnOrder.push("_emptyCol")
@@ -86,6 +51,43 @@ export class NhGroupedMatTable {
     this.dataSource.paginator = this.paginator;
   }
 
+  initData(dataArr: Group<ItemData>[]) {
+    let id = 0;
+    let itemId = 0;
+
+    for (let i = 0; i < dataArr.length; ++i) {
+      const group = dataArr[i];
+      const groupId = id;
+
+      this._data.push(
+        new GroupItem(
+          i,
+          group.name,
+          group.isExpanded,
+          id,
+          group.items.length
+        )
+      );
+
+      for (let j = 0; j < group.items.length; ++j) {
+        ++id;
+        
+        const item: DataItem = {
+          data: group.items[j],
+          groupId,
+          id: id,
+          isExpanded: true,
+          itemId,
+          relativeIndex: j
+        };
+
+        this._data.push(item);
+        ++itemId;
+      }
+      ++id;
+    }
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -94,73 +96,37 @@ export class NhGroupedMatTable {
       this.dataSource.paginator.firstPage();
   }
 
-  // TODO add "expanded" filter or something to hide the unexpended entries
-
-  expandRow(event: GroupItem) {
+  toggleRow(event: GroupItem) {
     event.isExpanded = !event.isExpanded;
 
-    // console.log(event)
-
-    for (let i = event.id + 1; (this._data[i] as Item)["groupId"] == event.id; ++i) {
-      // this._data[i].isExpanded = event.isExpanded;
+    for (let i = event.id + 1; (this._data[i] as DataItem)["groupId"] == event.id; ++i)
       this.dataSource.data[i].isExpanded = event.isExpanded
-    }
-
-    // this.refreshFilter();
   }
 
-  isGroup(index: number, item: GroupItem | Item): boolean {
+  isGroup(_: number, item: GroupItem | DataItem): boolean {
     if ((item as GroupItem).isGroup == true) return true
     return false;
-  }
-
-  isExpanded(index: number, item: GroupItem | Item) {
-    if (!Object.hasOwn(item, "isGroup")) return item.isExpanded;
-    else return false;
-  }
-
-  refreshFilter() {
-    this.dataSource.data.filter((val) => {
-      // console.log({ val, idk: Object.hasOwn(val, "isGroup") || val.isExpanded })
-      if (Object.hasOwn(val, "isGroup") || val.isExpanded) return val;
-    })
   }
 
   filterFunc(data: any, filter: string): boolean {
     if (!filter) return true;
     if (Object.hasOwn(data, "isGroup")) return true;
 
-    // const group = this.dataSource.data[data.groupId] as GroupItem;
-    // const findIndex = (val: number): number =>
-    //   (group.children as number[]).findIndex((value) => value == val);
-    
-    // const dataIndex = findIndex(data.groupId);
-
     for (let [_, value] of Object.entries(data.data)) {
-      // if (typeof value != "string" && typeof value != "number") continue;
-
       if (
         typeof value == "number" &&
         value.toString().startsWith(filter)
-      ) {
-        console.log(data)
-        return true
-      }
+      ) return true
       else if (
         typeof value == "string" &&
         value.toLowerCase().startsWith(filter.toLocaleLowerCase())
-      ) {
-        console.log(data)
-        return true;
-      }
-
+      ) return true;
     }
 
-    // if (dataIndex) group.children.splice(dataIndex, 1);
     return false;
   }
 
-  sortFunc(items: Item[], sort: MatSort): Item[] {
+  sortFunc(items: DataItem[], sort: MatSort): DataItem[] {
     if (!sort.active || sort.direction == '') return items;
 
     return items.sort((a, b) => {
@@ -183,7 +149,6 @@ class GroupItem {
     this.name = name;
     this.id = id;
     this.numChildren = numChildren ?? 0;
-    // this.children = [];
   }
   name: string;
   isExpanded: boolean;
@@ -191,14 +156,13 @@ class GroupItem {
   groupId: number;
   numChildren: number;
   id: number;
-  // children: number[];
 }
 
 interface ItemData {
   [key: string]: any,
 }
 
-interface Item {
+interface DataItem {
   data: ItemData,
   id: number,
   groupId: number,
